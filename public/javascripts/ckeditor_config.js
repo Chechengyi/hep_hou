0165b27a8396f9ee6ac9d5b39c2fcccb47a6dcfa
Chechengyi <984593931@qq.com>
2017-12-29 15:16:02 +0800

    第一次提交，其实已经基本做完了

---
+++ public/javascripts/ckeditor_config.js
@@ -0,0 +1,27 @@
+CKEDITOR.editorConfig = function( config )
+{
+    config.plugins = 'about,a11yhelp,basicstyles,bidi,blockquote,clipboard,colorbutton,colordialog,contextmenu,div,elementspath,enterkey,entities,filebrowser,find,flash,floatingspace,font,format,forms,horizontalrule,htmlwriter,image,iframe,indent,justify,link,list,liststyle,magicline,maximize,newpage,pagebreak,pastefromword,pastetext,preview,print,removeformat,resize,save,scayt,selectall,showblocks,showborders,smiley,sourcearea,specialchar,stylescombo,tab,table,tabletools,templates,toolbar,undo,wsc,wysiwygarea';
+    config.toolbarGroups = [
+        { name: 'document',    groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+        { name: 'forms' },
+        '/',
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+        { name: 'links' },
+        { name: 'insert' },
+        '/',
+        { name: 'styles' },
+        { name: 'colors' },
+        { name: 'tools' },
+        { name: 'others' }
+    ];
+    config.disableNativeSpellChecker = false ;
+    config.scayt_autoStartup = false;
+    config.font_names = '宋体/宋体;黑体/黑体;仿宋/仿宋_GB2312;楷体/楷体_GB2312;隶书/隶书;幼圆/幼圆;'+ config.font_names ;
+    config.contentsCss = CKEDITOR.getUrl( 'mycss.css' );
+    config.bodyClass='view';
+    config.extraPlugins = 'colorbutton, panelbutton, floatbutton, panel,button,colordialog,dialogui'
+    config.specialChars = config.specialChars.concat([['　','插入全角空格符']]);
+};
